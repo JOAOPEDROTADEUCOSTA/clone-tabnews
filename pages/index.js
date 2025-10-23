03dc1f2c67490a55848ae3539f251c2802d74ae1
JOAOPEDROTADEUCOSTA <163213040+JOAOPEDROTADEUCOSTA@users.noreply.github.com>
2025-10-23 04:20:41 +0000

    Adiciona scripts `lint:cheak` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>teste 3 mas agora vendo a diff</h1>
+function Home() {
+  return <h1>teste 3 mas agora vendo a diff</h1>;
 }
 
-export default Home; 
+function teste() {
+  console.log("teste indentação errada");
+}
+
+export default Home;
